56c696aa7679417fe4c9d80245c7884daa99377e
Ucha Javakhsivhili <ucha.javakhishvili@wandio.com>
2020-04-07 14:43:43 +0400

    app setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require("hbs");
 const utils = require("./utils/utils");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectory = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -76,6 +77,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000...");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}...`);
 });
